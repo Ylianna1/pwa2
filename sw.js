b978a0880973760306c97a25b7e2b505741db11d
Ylianna1 <95632183+Ylianna1@users.noreply.github.com>
2022-05-20 14:46:09 +0300

    SW

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+        caches.open("static")
+            .then(function(cache){
+                console.log("precaching");
+                cache.add('/index.html');
+                cache.add('/images/Logo.png');
+                cache.add('/bootstrap-4.0.0-dist/bootstrap.css');
+                cache.add('/Contact_us.html');
+                cache.add('/Tomato_all_vue.html');
+                cache.add('/Tomato_all.html');
+                cache.add('/Tomato_one.html');
+                cache.add('/Tomato_one1.html');
+                cache.add('/Contact_us.html');
+            })
+    );
 });
 
 self.addEventListener('activate', function(event) {
@@ -8,6 +23,14 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log('[Service Worker] Fetching Service Worker ...', event);
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response){
+                if (response)
+                    return response;
+                else
+                    return fetch(event.request);
+            }
+        )
+    );   
 });
