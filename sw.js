293b7c666e860dc8a1adce26846e40c6840b4b2c
Ylianna1 <95632183+Ylianna1@users.noreply.github.com>
2022-05-23 15:31:58 +0300

    Fetch

--- sw.js
+++ sw.js
@@ -13,6 +13,7 @@ self.addEventListener('install', function(event) {
                 cache.add('/Tomato_one.html');
                 cache.add('/Tomato_one1.html');
                 cache.add('/Contact_us.html');
+                cache.add('/Fetch.html');
             })
     );
 });
@@ -34,3 +35,31 @@ self.addEventListener('fetch', function(event) {
         )
     );   
 });
+
+const cat_result= document.getElementById('cat_result');
+const dog_result= document.getElementById('dog_result');
+const cat_btn= document.getElementById('cat_btn');
+const dog_btn= document.getElementById('dog_btn');
+
+cat_btn.addEventListener('click', getRandomCat)
+dog_btn.addEventListener('click', getRandomDog)
+
+function getRandomCat() {
+    fetch('https://randomfox.ca/floof/')
+        .then(res => res.json())
+        .then(data => {            
+            cat_result.innerHTML = `<img src="${data.image}"/>`
+        })
+}
+
+function getRandomDog() {
+    fetch('https://random.dog/woof.json')
+        .then(res => res.json())
+        .then(data => {
+            if (data.url.includes('.mp4')){
+                getRandomDog()
+            } else{
+            dog_result.innerHTML = `<img src="${data.url}"/>`
+            }
+        })
+}
